755fcea253ae83b9783e603dc53360155f209fd6
HeavyGunner17 <heavygunner17alv@gmail.com>
2023-12-07 00:26:15 -0300

    .

--- src/Paginas/Categoria.jsx
+++ src/Paginas/Categoria.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
